e31fc41138ad2be28f9c66a55537cab97d0b3015
hadriensevel <hadrien.sevel@outlook.com>
2023-10-11 19:23:50 +0200

    Refactor autocomplete code

--- website/js/autocomplete.js
+++ website/js/autocomplete.js
@@ -1,99 +1,85 @@
+// Global variables
+let chosenStationPopup = null;
+let searchBox = document.getElementById('searchBox');
 
-
-let search_options_dropdown_id = '#stations_dropdown';
-
-search_options_dropdown = UIkit.dropdown(search_options_dropdown_id, {
-  "pos": "bottom-left",
-  "mode": "click",
-  "animation": true
+// Search dropdown options
+const search_options_dropdown_id = '#stations_dropdown';
+const search_options_dropdown = UIkit.dropdown(search_options_dropdown_id, {
+    'pos': 'bottom-left',
+    'mode': 'click',
+    'animation': true
 });
 
 // Triggered when the dropdown is shown
-UIkit.util.on(search_options_dropdown_id, 'show', function () {
-  document.getElementById('stations_dropdown').classList.add('active');
-  console.log('showed');
-});
-
-// Triggered when the dropdown is hidden
-UIkit.util.on(search_options_dropdown_id, 'hide', function () {
-  console.log('hidden');
-});
-
-
-
-function pascalize(str) {
-  return str.replace(/(\w)(\w*)/g, function (g0, g1, g2) { return g1.toUpperCase() + g2.toLowerCase(); });
+function addDropdownEventListeners() {
+    // Triggered when the dropdown is shown
+    UIkit.util.on(search_options_dropdown_id, 'show', function () {
+        document.getElementById('stations_dropdown').classList.add('active');
+    });
 }
 
+// Triggered when the user types in the search box
+function addSearchBoxEventListener() {
+    searchBox.addEventListener('input', () => {
 
-let searchBox = document.getElementById("searchBox");
+        // open the dropdown if it is not already open
+        if (!UIkit.dropdown(search_options_dropdown_id).isActive()) {
+            UIkit.dropdown(search_options_dropdown_id).show();
+        }
 
+        const input = searchBox.value.toLowerCase();
+        const autocompleteSuggestions = station_data.filter((item) => item['stop_name'].toLowerCase().startsWith(input));
 
-searchBox.addEventListener("input", function () {
+        populate_dropdown_from_dataset(autocompleteSuggestions, searchBox);
+    });
+}
 
-  // open the dropdown if it is not already open
-  if (!UIkit.dropdown(search_options_dropdown_id).isActive()) {
-    UIkit.dropdown(search_options_dropdown_id).show();
-  }
-
-  let input = searchBox.value.toLowerCase();
-  console.log(input)
-
-
-  let autocompleteSuggestions = station_data.filter(function (item) { 
-       return item["stop_name"].toLowerCase().startsWith(input);
-  });
-
-  populate_dropdown_from_dataset(autocompleteSuggestions);
-});
-
-
-
-
-
+// Populate the dropdown with the suggestions
 function populate_dropdown_from_dataset(dataset) {
-  let dropdown_content = document.getElementById('stations_dropdown_content');
-  let dropdown_content_html = '';
-  let MAX_RESULTS = 4;
-  let table_length = dataset.length > MAX_RESULTS ? MAX_RESULTS : dataset.length;
-  for (let i = 0; i < table_length; i++) {
-    let station = dataset[i];
-    dropdown_content_html += '<tr onclick="displayStation(' + station["id"] + ')"><td>' + station['stop_name'] + '</td></tr>';
-  }
-  if (dataset.length === 0) {
-    dropdown_content_html = '<tr><td class="uk-text-center">Sorry, we couldn\'t find any station matching your search</td></tr>';
-  }
-  dropdown_content.innerHTML = dropdown_content_html;
+    const dropdown_content = document.getElementById('stations_dropdown_content');
+
+    let dropdown_content_html = '';
+    const MAX_RESULTS = 4;
+
+    dataset.slice(0, MAX_RESULTS).forEach(station => {
+        dropdown_content_html += `<tr onclick="displayStation(${station.id})"><td>${station.stop_name}</td></tr>`;
+    });
+
+    if (dataset.length === 0) {
+        dropdown_content_html = '<tr><td class="uk-text-center">Sorry, we couldn\'t find any station matching your search</td></tr>';
+    }
+
+    dropdown_content.innerHTML = dropdown_content_html;
 }
 
-var chosenStationPopup = null
-
-
+// Display the popup of the station
 function displayStation(id) {
-  console.log("displayStation(", id, ") triggered")
-  // need to retrieve the circle corresponding to this station id in the mapbox map
-  station = station_data[id];
-  coordinates = [station['stop_lon'], station['stop_lat']];
-  description = "Station"
+    // need to retrieve the circle corresponding to this station id in the mapbox map
+    const station = station_data[id];
+    const coordinates = [station['stop_lon'], station['stop_lat']];
 
-  if (chosenStationPopup !== null) {
-    chosenStationPopup.remove();
-  }
+    if (chosenStationPopup !== null) {
+        chosenStationPopup.remove();
+    }
 
-  // close dropdown
-  UIkit.dropdown(search_options_dropdown_id).hide(animation = false);
+    // close dropdown
+    UIkit.dropdown(search_options_dropdown_id).hide(animation = false);
 
-  // add in text field the station stop
-  searchBox.value = station['stop_name'];
+    // add in text field the station stop
+    searchBox.value = station['stop_name'];
 
-  chosenStationPopup = new mapboxgl.Popup()
-    .setLngLat(coordinates)
-    .setHTML(generatePopupHTML(station))
-    .addTo(map);
+    chosenStationPopup = new mapboxgl.Popup()
+        .setLngLat(coordinates)
+        .setHTML(generatePopupHTML(station))
+        .addTo(map);
 
-  map.flyTo({
-    center: coordinates,
-    zoom: 8
-  });
-
+    map.flyTo({
+        center: coordinates,
+        zoom: 8
+    });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    addDropdownEventListeners();
+    addSearchBoxEventListener();
+});
